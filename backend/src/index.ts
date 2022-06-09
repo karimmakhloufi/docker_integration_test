ba6621ada263e1b6c97e98d73cee2ab91cb3d068
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2022-06-09 11:01:37 +0200

    apollo mongo mongoose 101

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,43 @@
-console.log("hello from ts");
+import { ApolloServer, gql } from "apollo-server";
+import { Schema, model, connect } from "mongoose";
+
+const options = {
+  authSource: "admin",
+};
+
+connect("mongodb://root:root@database:27017/books", options);
+
+interface IBook {
+  title: string;
+  author: string;
+}
+
+const bookSchema = new Schema<IBook>({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+});
+
+const Book = model<IBook>("Book", bookSchema);
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    books: async () => await Book.find(),
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
